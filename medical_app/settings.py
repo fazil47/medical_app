ab1fd7ac0c153bfc758bb11506defff2438faeef
Fazil Babu <fazil47babu@gmail.com>
2021-09-27 21:31:14 +0530

    Not running in debug mode anymore

--- medical_app/settings.py
+++ medical_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0s!o1tq4mipus4@sx)##i0!_s5o7e**b&hg0!ljxt+&ewz5rar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
